820465d82f7a9200ee1e7bc2ed04323b63e9d2a7
Sayyam Jain <143936791+Sayyamjain23@users.noreply.github.com>
2025-02-17 17:55:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
-  AiFillGithub, 
+  AiFillGithub 
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
 
